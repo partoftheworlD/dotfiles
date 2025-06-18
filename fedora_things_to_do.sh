8b8abfa3e7a5e4c678bb611fe2b51caf59bc78be
partoftheworlD <partoftheworld@vivaldi.net>
2025-06-18 18:24:12 +0300

    Update fedora_things_to_do.sh

--- fedora_things_to_do.sh
+++ fedora_things_to_do.sh
@@ -144,7 +144,7 @@ dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld -y
 # Install essential applications
 color_echo "yellow" "Installing essential applications..."
 dnf install -y tmux btop git wget curl jetbrains-mono-fonts rsms-inter-fonts duperemove btrfs-assistant neovim gamescope lutris steam distrobox gamemode kernel-cachyos kernel-cachyos-devel-matched cachyos-settings
-flatpak install -y spotify heroic
+flatpak install -y spotify heroic protonplus
 color_echo "green" "Essential applications installed successfully."
 
 # Install Internet & Communication applications
