971c346cce5f59f94d35e9734c504c8a0dd7d44e
Partoo <partoftheworld@vivaldi.net>
2025-12-28 18:15:01 +0300

    More gnome settings

--- post-install/gnome_settings_nonroot.sh
+++ post-install/gnome_settings_nonroot.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+gsettings set org.gnome.desktop.interface cursor-theme 'Yaru'
+gsettings set org.gnome.desktop.interface document-font-name 'Ubuntu 11'
 gsettings set org.gnome.desktop.interface font-antialiasing 'rgba'
 gsettings set org.gnome.desktop.interface font-hinting 'full'
+gsettings set org.gnome.desktop.interface font-name 'Ubuntu 11'
+gsettings set org.gnome.desktop.interface gtk-theme 'Yaru'
+gsettings set org.gnome.desktop.interface icon-theme 'Yaru'
 gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrains Mono 11'
-gsettings set org.gnome.desktop.interface document-font-name 'Ubuntu 11'
-gsettings set org.gnome.settings-daemon.plugins.housekeeping donation-reminder-enabled false
-gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
-gsettings set org.gnome.desktop.screensaver lock-enabled false
-gsettings set org.gnome.desktop.peripherals.keyboard remember-numlock-state true
+gsettings set org.gnome.desktop.interface toolkit-accessibility false
 gsettings set org.gnome.desktop.peripherals.keyboard numlock-state true
+gsettings set org.gnome.desktop.peripherals.keyboard remember-numlock-state true
+gsettings set org.gnome.desktop.screensaver lock-enabled false
+gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
+gsettings set org.gnome.settings-daemon.plugins.housekeeping donation-reminder-enabled false
 gsettings set org.gnome.shell favorite-apps "['brave-browser.desktop', 'obsidian.desktop', 'spotify-launcher.desktop', 'steam.desktop', 'com.obsproject.Studio.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Software.desktop', 'org.gnome.TextEditor.desktop', 'org.gnome.Calculator.desktop']"
